b46e9d6aa58394ce7b9d4d1b78d65682a3e41058
Mike Tellinghuisen <mtellin@gmail.com>
2020-06-26 15:55:55 -0500

    Initial Commit

---
+++ REST API Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  REST API Tutorial
+//
+//  Created by Mike Tellinghuisen on 6/26/20.
+//  Copyright © 2020 Mike Tellinghuisen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
